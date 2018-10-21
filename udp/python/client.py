0f2ad27983a1ad9c15f236b41511028300983c0e
JL-Cheng <chengjl16@163.com>
2018-10-21 21:52:43 +0800

    完成UDP部分

--- udp/python/client.py
+++ udp/python/client.py
@@ -3,11 +3,11 @@ import socket
 size = 8192
  
 try:
-  msg = raw_input()
-  sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-  sock.sendto(msg, ('localhost', 9876))
-  print sock.recv(size)
-  sock.close()
+	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	for num in range(0,51):
+		sock.sendto(str.encode(str(num)), ('localhost', 9876))
+		print (bytes.decode(sock.recv(size))+'\n')
+	sock.close()
  
 except:
-  print "cannot reach the server"
+	print ("cannot reach the server")
--- udp/python/server.py
+++ udp/python/server.py
@@ -2,12 +2,15 @@ import socket
 
 size = 8192
 
+count = 1
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind(('', 9876))
+sock.bind(('0.0.0.0', 9876))
 
 try:
-  while True:
-    data, address = sock.recvfrom(size)
-    sock.sendto(data.upper(), address)
+	while True:
+		data, address = sock.recvfrom(size)
+		data = str(count) + ' ' + bytes.decode(data)
+		sock.sendto(str.encode(data.upper()), address)
+		count = count + 1
 finally:
-  sock.close()
+	sock.close()
